848152cdbb084d88b8b010d2d3fc4bb4527585f2
erhic <ericngugi24@gmail.com>
2023-07-28 17:30:49 +0300

    added a navabar and created routes and student components and install bootstrap

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,28 @@ import HelloWorld from './components/HelloWorld.vue'
 
 
     <div class="wrapper">
+      <nav class="navbar navbar-expand-xl navbar-info bg-info">
+        <div class="container-fluid">
+          <RouterLink class="navbar-brand" to="/">Student Manager</RouterLink>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarWithDropdown"
+            aria-controls="navbarWithDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse show" id="navbarWithDropdown">
+            <ul class="navbar-nav ms-auto">
+              <li class="nav-item">
+                <RouterLink class="nav-link active" aria-current="page" to="/">Home</RouterLink>
+              </li>
+              <li class="nav-item">
+                <RouterLink class="nav-link" to="/about">About Us</RouterLink>
+              </li>
+              <li class="nav-item">
+                <RouterLink class="nav-link" to="/students">Students</RouterLink>
+              </li>
 
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+            </ul>
+          </div>
+        </div>
       </nav>
     </div>
   </header>
